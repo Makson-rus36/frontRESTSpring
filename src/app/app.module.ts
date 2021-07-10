cb2d39f2d7fe2db0687decb9efd407fc1237a38c
Maks <maksim.t00@gmail.com>
2021-07-11 00:20:05 +0300

    Add init message component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {MessageComponent} from "./message/message.component";
+import {RouterModule, Routes} from "@angular/router";
+
+const appRoutes: Routes=[
+  {path:"message", component:MessageComponent}
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MessageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
