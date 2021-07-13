7deaa017a7addcf37d05582dd5d7fa40eb7467ba
Maks <maksim.t00@gmail.com>
2021-07-13 18:10:57 +0300

    prep 3

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static('.dist/frontRESTSpring'));
+
+app.get('/*', (req, res)=>res.sendFile('index.html',{root: 'dist/frontRESTSpring'}),);
+
+app.listen(process.env.PORT||4200)
